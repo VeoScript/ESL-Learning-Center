146333be6d03b7569bd097c9fe03fc70b914a889
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-06-02 14:47:37 +0800

    basic design

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <carousel></carousel>
+    <displaycards></displaycards>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+  components:{   
+    carousel: () => import('@/components/Carousel.vue'),
+    displaycards: () => import('@/components/DisplayCards.vue')
+  }
+};
+</script>
