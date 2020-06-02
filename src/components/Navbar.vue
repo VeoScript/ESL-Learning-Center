8ce6ef379bea6430b46edf9b5d00078faa1277a2
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-06-02 14:56:54 +0800

    link updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,12 +9,12 @@
         <b-navbar-nav class="ml-auto">
           <router-link to="/" :class="$route.path == '/' ? 'nav-link active' : 'nav-link'">Home</router-link>
           <router-link to="/" :class="$route.path == '/freedomwall' ? 'nav-link active' : 'nav-link'">About</router-link>
-          <router-link to="/" :class="$route.path == '/freedomwall' ? 'nav-link active' : 'nav-link'">Services</router-link>
-          <router-link to="/" :class="$route.path == '/about' ? 'nav-link active' : 'nav-link'">Resources</router-link>
-          <router-link to="/" :class="$route.path == '/about' ? 'nav-link active' : 'nav-link'">FAQ</router-link>
-          <router-link to="/" :class="$route.path == '/about' ? 'nav-link active' : 'nav-link'">Contact Me</router-link>
           <router-link to="/" :class="$route.path == '/about' ? 'nav-link active' : 'nav-link'">Register</router-link>
           <router-link to="/" :class="$route.path == '/about' ? 'nav-link active' : 'nav-link'">Login</router-link>
+          <!-- <router-link to="/" :class="$route.path == '/freedomwall' ? 'nav-link active' : 'nav-link'">Services</router-link>
+          <router-link to="/" :class="$route.path == '/about' ? 'nav-link active' : 'nav-link'">Resources</router-link>
+          <router-link to="/" :class="$route.path == '/about' ? 'nav-link active' : 'nav-link'">FAQ</router-link>
+          <router-link to="/" :class="$route.path == '/about' ? 'nav-link active' : 'nav-link'">Contact Me</router-link> -->
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
