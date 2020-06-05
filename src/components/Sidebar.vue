ed9bf263587c8c8de6e98ec2dfb1cc5371bc0d49
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2020-06-05 10:02:27 +0800

    fixed debugs 1

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,11 +2,11 @@
   <div>
     <b-sidebar id="sidebar-right" title="Menu" right shadow backdrop>
       <b-list-group id="list-group">
-        <b-list-group-item to="/">Home</b-list-group-item>
-        <b-list-group-item to="/about">About</b-list-group-item>
-        <b-list-group-item to="/about">Lessons</b-list-group-item>
-        <b-list-group-item to="/about">Teachers</b-list-group-item>
-        <b-list-group-item to="/about">Contacts</b-list-group-item>
+        <b-list-group-item href="/">Home</b-list-group-item>
+        <b-list-group-item href="/">About</b-list-group-item>
+        <b-list-group-item href="/">Lessons</b-list-group-item>
+        <b-list-group-item href="/">Teachers</b-list-group-item>
+        <b-list-group-item href="/">Contacts</b-list-group-item>
         <b-list-group-item>
           <b-button-group size="sm">
             <b-button>Register</b-button>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,22 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <!-- <b-container>
-      <tabs></tabs>
-    </b-container> -->
   </div>
 </template>
 
-<script>
-export default {
-  components: {
-    //Tabs: () => import("@/components/Tabs.vue")
-  }
-}
-</script>
-
 <style lang="scss">
-  .about{
-    margin-top: 50px;
-  }
+.about {
+  margin-top: 50px;
+}
 </style>
