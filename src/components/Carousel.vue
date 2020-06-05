136dad82ba5e8014c79c338219ffc3960cd828bf
acatzk <joshuaimalay@gmail.com>
2020-06-05 08:49:30 +0800

    removed v-model and functions

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,7 +2,6 @@
   <div>
     <b-carousel
       id="carousel-1"
-      v-model="slide"
       :interval="4000"
       controls
       indicators
@@ -10,8 +9,6 @@
       img-width="1024"
       img-height="480"
       style="text-shadow: 1px 1px 2px #333;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
     >
       <b-carousel-slide>
         <template v-slot:img>
