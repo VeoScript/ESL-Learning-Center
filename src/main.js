03a086ccdbec2c6fefd15b199a89ec56f92e9bc8
acatzk <joshuaimalay@gmail.com>
2020-06-05 10:46:34 +0800

    import vue-page-transition

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import "mutationobserver-shim";
 import Vue from "vue";
 import "./plugins/bootstrap-vue";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import VuePageTransition from 'vue-page-transition'
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -10,6 +11,7 @@ import store from "./store";
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
+Vue.use(VuePageTransition)
 
 new Vue({
   router,
