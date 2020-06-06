5914ae90f92247b3493b91b46c689876f9e322c8
acatzk <joshuaimalay@gmail.com>
2020-06-06 11:35:14 +0800

    changed sidebar icons and adde class active style

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,37 +1,33 @@
 <template>
   <div>
     <b-sidebar id="sidebar-right" title="Menu" right shadow backdrop backdrop-variant="transparent">
-      <b-list-group id="list-group">
-        <b-list-group-item to="/" id="text-style">
-          <b-icon id="icon" icon="house-fill"></b-icon> Home
+      <b-list-group>
+        <b-list-group-item style="border-radius: none;" :class="$route.path === '/' ? 'active' : ''" to="/">
+          <b-icon icon="house"></b-icon> Home
         </b-list-group-item>
-        <b-list-group-item to="/about">
-          <b-icon id="icon" icon="person-square"></b-icon> About
+        <b-list-group-item :class="$route.path === '/about' ? 'active' : ''" to="/about">
+          <b-icon icon="person-check"></b-icon> About
         </b-list-group-item>
-        <b-list-group-item to="/about">
-          <b-icon id="icon" icon="grid1x2-fill"></b-icon> Lessons
+        <b-list-group-item :class="$route.path === '/lessons' ? 'active' : ''" to="/lessons">
+          <b-icon icon="receipt"></b-icon> Lessons
         </b-list-group-item>
-        <b-list-group-item to="/about">
-          <b-icon id="icon" icon="person-bounding-box"></b-icon> Teachers
+        <b-list-group-item :class="$route.path === '/teachers' ? 'active' : ''" to="/teachers">
+          <b-icon icon="people"></b-icon> Teachers
         </b-list-group-item>
-        <b-list-group-item to="/about">
-          <b-icon id="icon" icon="person-lines-fill"></b-icon> Contacts
+        <b-list-group-item :class="$route.path === '/contacts' ? 'active' : ''" to="/contacts">
+          <b-icon icon="file-richtext"></b-icon> Contacts
         </b-list-group-item>
-        <b-list-group-item class="d-flex justify-content-center">
-          <b-button-group size="sm">
-            <b-button class="defaultstyle">
-              <b-icon id="icon" icon="person-plus-fill"></b-icon> Register
-            </b-button>
-            <b-button class="defaultstyle">
-              <b-icon id="icon" icon="person-circle"></b-icon> Login
-            </b-button>
-          </b-button-group>
+        <b-list-group-item to="/register" :class="$route.path === '/register' ? 'active' : ''">
+           <b-icon icon="person-plus"></b-icon> Register
         </b-list-group-item>
+        <b-list-group-item to="/login" :class="$route.path === '/login' ? 'active' : ''">
+           <b-icon icon="person"></b-icon> Login
+        </b-list-group-item>
+        <!-- <b-list-group-item class="d-flex justify-content-center align-items-center business-logo">
+            <b-img fluid src="@/assets/photos/logo.png" width="100"></b-img>
+            <p class="cname">Victor Air Class Solutions, Inc.</p>
+        </b-list-group-item> -->
       </b-list-group>
-      <p class="copyright mx-2 my-2">
-        &copy;2020 Victor Air Class Solutions, Inc. / All Rights Reserved.<br />
-        Designed and Developed by VEOSCRIPT and ACATZK.
-      </p>
     </b-sidebar>
   </div>
 </template>
@@ -43,22 +39,20 @@ export default {
 </script>
 
 <style scoped>
+
 #list-group {
   border-radius: 0;
-  font-weight: bolder !important;
-}
-#icon {
-  font-weight: bolder !important;
 }
 
-.defaultstyle {
-  border: 1px solid teal;
-  background-color: #fff;
-  color: teal;
+.active {
+  background: rgb(44, 204, 204) !important;
+  border: none;
+  color: white;
 }
-.defaultstyle:focus {
-  box-shadow: none;
-  background: teal;
+
+.business-logo {
+  background: rgb(204, 204, 204);
+  border: none;
   color: white;
 }
 </style>
