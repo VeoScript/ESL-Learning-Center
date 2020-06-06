e424a826b46ebc159d8ab8c434ca63f071416d73
acatzk <joshuaimalay@gmail.com>
2020-06-06 12:01:17 +0800

    create hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="hero">
+        <b-container>
+            <b-row class="d-flex justify-content-center align-items-center">
+                <b-col col="6">
+                    <b-img src="@/assets/logo.png"/>
+                </b-col>
+                <b-col col="6">
+                
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.hero {
+    background: teal;
+    height: 90vh;
+}
+
+</style>
