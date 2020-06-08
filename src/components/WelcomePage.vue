9a436e8f84aa7a52b7fa47e7c4b7294bcc272705
acatzk <joshuaimalay@gmail.com>
2020-06-08 08:40:07 +0800

    changed font-size paragraph in media query

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -3,12 +3,11 @@
         <b-container>
             <b-row class="d-flex justify-content-center align-items-center hero-section text-center ma-3">
                 <b-col md="6">
-                    <b-img class="side-image" width="200" src="@/assets/photos/communication.png" ></b-img>
+                    <b-img class="side-image" width="180" src="@/assets/photos/communication.png" ></b-img>
                 </b-col>
                 <b-col lg="6" class="text-center">
                     <h2>Air Class Solution, Inc.</h2>
-                    <h4>Learning English Language Through Video Calls</h4>
-                    <p>ESL Learning Center Putting Children First. Preparing children for success in life.</p>
+                    <p>Learning English Language Through Video Calls. Putting Children First. Preparing children for success in life.</p>
                     <b-button pill variant="danger" size="lg">Learn More</b-button>
                 </b-col>
             </b-row>
@@ -22,7 +21,7 @@
     background-repeat: no-repeat;
     background-size: 100% 100%;
         // filter: brightness(95%);
-    height: 85vh;
+    padding: 100px 0;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -32,7 +31,7 @@
     h2 {
         font-weight: bold;
     }
-    h2, h4, p {
+    h2, p {
         color: white;
     }
     p {
@@ -41,8 +40,21 @@
 }
 
 @media only screen and (min-width: 1000px) {
-  .side-image {
-      width: 400px;
+  .hero-section {   
+    .side-image {
+        width: 400px;
+    }
+  }
+}
+
+@media only screen and (max-width: 1000px) {
+  .hero-section {
+    h2 {
+        font-size: 20px;
+    }
+    p {
+        font-size: 13px;
+    }
   }
 }
 </style>
