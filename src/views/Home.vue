275fa3440c2145a3418d14d70c0fab5babbddfd2
acatzk <joshuaimalay@gmail.com>
2020-06-08 11:08:58 +0800

    added data function passed thru props

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <welcome-page />
+    <welcome-page 
+       :sideImage="sideImage"
+       :title="title"
+       :description="description"
+       :buttonText="buttonText"
+    />
     <vision-mission />
     <connect-with-us />
     <horizontal-list />
@@ -17,6 +22,13 @@ export default {
     FootBar: () => import("@/components/FootBar"),
     ConnectWithUs: () => import("@/components/ConnectWithUs"),
     HorizontalList: () => import('@/components/HorizontalList')
-  }
+  },
+
+  data: () => ({
+      sideImage: require('@/assets/photos/communication.png'),
+      title: 'Air Class Solution, Inc.',
+      description: 'Learning English Language Through Video Calls. Putting Children First. Preparing children for success in life.',
+      buttonText: 'Learn More'
+  })
 };
 </script>
