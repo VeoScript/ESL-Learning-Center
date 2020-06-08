e2c9382df767153339a97e0f20728ecd11327e8c
acatzk <joshuaimalay@gmail.com>
2020-06-08 11:14:04 +0800

    added props text and image

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="about">
-     <welcome-page />
+     <welcome-page 
+       :sideImage="sideImage"
+       :title="title"
+       :description="description"
+       :buttonText="buttonText"
+     />
   </div>
 </template>
 
@@ -10,7 +15,14 @@ export default {
 
   components: {
     WelcomePage: () => import('@/components/WelcomePage')
-  }
+  },
+
+  data: () => ({
+    sideImage: require('@/assets/photos/communication.png'),
+    title: 'About Our Company',
+    description: 'We are online learning language tutor. Any where in the world.  Quality education is with English school tradition',
+    buttonText: 'Enroll Now'
+  })
 }
 </script>
 
