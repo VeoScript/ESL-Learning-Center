11510292bd6b4838f559615191853b18c9173759
acatzk <joshuaimalay@gmail.com>
2020-08-19 08:57:24 +0800

    Added an object frontpage for the prop text pass thru

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <front-page 
-       :sideImage="sideImage"
-       :title="title"
-       :description="description"
-       :buttonText="buttonText"
+       :sideImage="frontpage.sideImage"
+       :title="frontpage.title"
+       :description="frontpage.description"
+       :buttonText="frontpage.buttonText"
     />
     <home-vision-mission-page />
     <home-connect-page />
@@ -14,19 +14,26 @@
 
 <script>
   export default {
+
     name: "Home",
+
+    data () {
+      return {
+         frontpage: {
+          sideImage: require('@/assets/photos/communication.png'),
+          title: 'Air Class Solution, Inc.',
+          description: 'Learning English Language Through Video Calls. Putting Children First. Preparing children for success in life.',
+          buttonText: 'Learn More'
+         }
+      }
+    },
+
     components: {
       FrontPage: () => import('@/components/mixins/FrontPage'),
       HomeVisionMissionPage: () => import('@/components/pages/home/HomeVisionMission'),
       HomeConnectPage: () => import('@/components/pages/home/HomeContactPage'),
       HorizontalList: () => import('@/components/HorizontalList')
-    },
+    }
 
-    data: () => ({
-        sideImage: require('@/assets/photos/communication.png'),
-        title: 'Air Class Solution, Inc.',
-        description: 'Learning English Language Through Video Calls. Putting Children First. Preparing children for success in life.',
-        buttonText: 'Learn More'
-    })
   }
 </script>
