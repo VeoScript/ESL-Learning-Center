2a0e38ea1b3e11125d92ffbd5a0f66e1e642953a
acatzk <joshuaimalay@gmail.com>
2020-08-19 10:19:08 +0800

    Created button and embeded icon to triggered to gotop

---
+++ src/components/mixins/ScrollToTop.vue
@@ -0,0 +1,28 @@
+<template>
+    <back-to-top bottom="50px" right="50px">
+        <b-button variant="primary" class="btn-to-top">
+            <i class="fa fa-chevron-up"></i>
+        </b-button>
+    </back-to-top>
+</template>
+
+<script>
+    export default {
+        
+        components: {
+            BackToTop: () => import('vue-backtotop')
+        }
+
+    }
+</script>
+
+<style scoped>
+    .btn-to-top {
+        width: 60px;
+        height: 60px;
+        padding: 10px 16px;
+        border-radius: 50%;
+        font-size: 22px;
+        line-height: 22px;
+    }
+</style>
