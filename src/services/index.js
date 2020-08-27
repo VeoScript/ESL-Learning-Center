531ca46a48e87db358c2853ce80c15b40c9793db
acatzk <joshuaimalay@gmail.com>
2020-08-27 12:33:29 +0800

    Import and export firebase init

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,11 @@
 import { apolloProvider } from './apollo'
+import { fb, db, auth, messaging, currentUser } from './firebase'
 
 export {
+    fb,
+    db,
+    auth,
+    messaging,
+    currentUser,
     apolloProvider
 }
