f76380c74c8525de1da18bb2fd9222bbd9183506
acatzk <joshuaimalay@gmail.com>
2020-08-27 15:51:44 +0800

    Update muation and added firebase_id and password in the column

--- src/graphql/mutations/students.js
+++ src/graphql/mutations/students.js
@@ -2,12 +2,12 @@ import gql from 'graphql-tag'
 
 
 export const ADD_STUDENT_MUTATION = gql`
-    mutation AddStudentMutation($firstname: String!, $lastname: String!, $email: String, $contact: String, $gender: String, $birth_date: String) {
-        insert_students(objects: {firstname: $firstname, lastname: $lastname, email: $email, contact: $contact, gender: $gender, birth_date: $birth_date}) {
+    mutation AddStudentMutation($firebase_id: String!, $firstname: String!, $lastname: String!, $email: String, $contact: String, $gender: String, $birth_date: String, $password: String!) {
+        insert_students(objects: {firebase_id: $firebase_id, firstname: $firstname, lastname: $lastname, email: $email, contact: $contact, gender: $gender, birth_date: $birth_date, password: $password}) {
             affected_rows
             returning {
                 id
             }
         }
-    }  
+    }
 `
