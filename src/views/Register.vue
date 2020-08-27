78ff5f1f2041942fbc61a1bd18f3044c7fcb7ec2
acatzk <joshuaimalay@gmail.com>
2020-08-27 11:57:53 +0800

    Added rules validation text fields using vuelidate

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,13 @@
 <template>
-    <b-modal id="register-modal" title="Registration" size="medium" hide-footer>
-        <b-form>
+    <b-modal 
+        id="register-modal" 
+        title="Student Registration" 
+        size="medium" 
+        hide-footer 
+        centered
+        content-class="shadow"
+    >
+        <b-form @submit.prevent="onClickSubmitForm">
             
             <b-row>
                 <b-col>
@@ -8,14 +15,27 @@
                         label="Firstname"
                     >
                         <b-form-input
+                            v-model.trim="$v.firstname.$model"
+                            :class="{ 'is-invalid' : $v.firstname.$error, 'is-valid' : !$v.firstname.$invalid }"
                         ></b-form-input>
+                        <div class="invalid-feedback feedback">
+                            <span v-if="!$v.firstname.required">Firstname is required</span>
+                            <span v-if="!$v.firstname.minLength">Firstname must have at least {{ $v.firstname.$params.minLength.min }} letters.</span>
+                            <span v-if="!$v.firstname.maxLength">Firstname must have at most {{ $v.firstname.$params.maxLength.max }} letters.</span>
+                        </div>
                     </b-form-group>
                 </b-col>
 
                 <b-col>
                     <b-form-group label="Lastname">
                         <b-form-input
+                            v-model.trim="$v.lastname.$model"
+                            :class="{ 'is-invalid' : $v.lastname.$error, 'is-valid' : !$v.lastname.$invalid }"
                         ></b-form-input>
+                        <div class="invalid-feedback feedback">
+                            <span v-if="!$v.lastname.required">Lastname is required</span>
+                            <span v-if="!$v.lastname.maxLength">Lastname must have at most {{ $v.lastname.$params.maxLength.max }} letters.</span>
+                        </div>
                     </b-form-group>
                 </b-col>
             </b-row>
@@ -24,14 +44,24 @@
                 <b-col>
                     <b-form-group label="Email">
                         <b-form-input
+                            v-model.trim="$v.email.$model"
+                            :class="{ 'is-invalid' : $v.email.$error, 'is-valid' : !$v.email.$invalid }"
                         ></b-form-input>
+                        <div class="invalid-feedback feedback">
+                            <span v-if="!$v.email.required">Email is required</span>
+                            <span v-if="!$v.email.email">Email is invalid.</span>
+                        </div>
                     </b-form-group>
                 </b-col>
-
                 <b-col>
                     <b-form-group label="Contact">
                         <b-form-input
+                            v-model.trim="$v.contact.$model"
+                            :class="{ 'is-invalid' : $v.contact.$error, 'is-valid' : !$v.contact.$invalid }"
                         ></b-form-input>
+                        <div class="invalid-feedback feedback">
+                            <span v-if="!$v.contact.required">Contact is required</span>
+                        </div>
                     </b-form-group>
                 </b-col>
             </b-row>
@@ -39,27 +69,38 @@
             <b-row>
                 <b-col>
                     <b-form-group label="Gender">
-                        <b-form-select v-model="genderSelected" :options="gender"></b-form-select>
+                        <b-form-select 
+                            :options="gender"
+                            v-model.trim="$v.genderSelected.$model"
+                            :class="{ 'is-invalid' : $v.genderSelected.$error, 'is-valid' : !$v.genderSelected.$invalid }"
+                        ></b-form-select>
+                        <div class="invalid-feedback feedback">
+                            <span v-if="!$v.genderSelected.required">Gender is required</span>
+                        </div>
                     </b-form-group>
                 </b-col>
                 <b-col>
                     <b-form-group label="Birth date">
                         <b-input-group class="mb-3">
                             <b-form-input
-                                v-model="birthdate"
+                                v-model.trim="$v.birthdate.$model"
                                 type="text"
                                 placeholder="YYYY-MM-DD"
                                 autocomplete="off"
+                                :class="{ 'is-invalid' : $v.birthdate.$error, 'is-valid' : !$v.birthdate.$invalid }"
                             ></b-form-input>
                             <b-input-group-append>
                                 <b-form-datepicker
-                                v-model="birthdate"
-                                button-only
-                                right
-                                @context="onContext"
+                                    v-model="birthdate"
+                                    button-only
+                                    right
+                                    @context="onContext"
                                 ></b-form-datepicker>
                             </b-input-group-append>
                         </b-input-group>
+                        <div class="invalid-feedback feedback">
+                            <span v-if="!$v.birthdate.required">Bithdate is required</span>
+                        </div>
                     </b-form-group>
                 </b-col>
             </b-row>
@@ -69,10 +110,15 @@
                     <b-button 
                         block
                         variant="primary"
+                        @click="onClickSubmitForm"
                     >Register</b-button>
                 </b-col>
                 <b-col>
-                    <b-button block @click="$bvModal.hide('register-modal')">Cancel</b-button>
+                    <b-button 
+                        block 
+                        @click="onClickResetForm"
+                        variant="outline-secondary"
+                    >Cancel</b-button>
                 </b-col>
             </b-row>
 
@@ -82,21 +128,53 @@
 
 <script>
 
+    import { toastAlertStatus } from '@/utils'
 
+    import { required, minLength, maxLength, email } from 'vuelidate/lib/validators'
 
     export default {
         name: 'register',
 
         data () {
             return {
-                genderSelected: null,
+                loading: false,
+                firstname: '',
+                lastname: '',
+                email: '',
+                contact: '',
+                genderSelected: '',
                 gender: [
                     { value: 'Male', text: 'Male' },
                     { value: 'Female', text: 'Female' }
                 ],
                 birthdate: '',
-                formatted: '',
-                birthdateSelected: ''
+                formatted: null,
+                birthdateSelected: null
+            }
+        },
+
+        validations: {
+            firstname: {
+                required,
+                minLength: minLength(3),
+                maxLength: maxLength(25)
+            },
+            lastname: {
+                required,
+                maxLength: maxLength(25)
+            },
+            email: {
+                required,
+                email
+            },
+            contact: {
+                required
+            },
+            genderSelected: {
+                required
+            },
+            birthdate: {
+                required
             }
         },
 
@@ -106,6 +184,37 @@
                 this.formatted = ctx.selectedFormatted
                 // The following will be an empty string until a valid date is entered
                 this.birthdateSelected = ctx.selectedYMD
+            },
+
+            onClickResetForm () {
+                this.firstname = null
+                this.lastname = null
+                this.email = null
+                this.contact = null
+                this.birthdate = null
+                this.genderSelected  = null
+                //this.$bvModal.hide('register-modal')
+                this.$v.$reset()
+            },
+
+            onClickSubmitForm () {
+                this.$v.$touch()
+
+                if (!this.$v.$invalid) {
+                    
+                    const { 
+                        firstname,
+                        lastname,
+                        email,
+                        contact,
+                        birthdate,
+                        genderSelected
+                    } = this.$data
+
+                    
+
+                   this.onClickResetForm()
+                }
             }
         }
     }
