c90b6cf35335d8e3d8113de91399336c5757ae9a
acatzk <joshuaimalay@gmail.com>
2020-08-29 11:58:10 +0800

    Protected the authenticted route and added new route

--- src/components/layouts/TheNavBar.vue
+++ src/components/layouts/TheNavBar.vue
@@ -13,26 +13,60 @@
         <b-collapse v-model="toggle" is-nav>
           <b-navbar-nav class="ml-auto">
             <b-navbar-nav class="d-flex justify-content-center align-items-center">
+
+              <!-- HOME PAGE -->
               <b-nav-item 
                 href="#" 
                 :class="$route.path === '/' ? 'active font-weight-bold' : ''" 
                 to="/"
+                v-if="!isLoggedIn"
               >
                   <b-icon 
                     :icon="$route.path === '/' ? 'house-fill' : 'house'">
                   </b-icon>
                   Home
-                </b-nav-item>
+              </b-nav-item>
+
+              <!-- DASHBOARD PAGE -->
+              <b-nav-item 
+                href="#" 
+                :class="$route.path === '/dashboard' ? 'active font-weight-bold' : ''" 
+                to="/dashboard"
+                v-if="isLoggedIn"
+              >
+                  <b-icon 
+                    :icon="$route.path === '/dashboard' ? 'house-fill' : 'house'">
+                  </b-icon>
+                  Dahsboard
+              </b-nav-item>
+
+              <!-- ABOUT PAGE -->
               <b-nav-item 
                 href="#" 
                 to="/about"
                 :class="$route.path === '/about' ? 'active font-weight-bold' : ''"
+                v-if="!isLoggedIn"
               >
                 <b-icon 
                   :icon="$route.path === '/about' ? 'person-check-fill' : 'person-check'">
                 </b-icon> 
                 About
               </b-nav-item>
+
+              <!-- OUR SERVICES PAGE -->
+              <b-nav-item 
+                href="#" 
+                to="/services"
+                :class="$route.path === '/services' ? 'active font-weight-bold' : ''"
+                v-if="!isLoggedIn"
+              >
+                <b-icon 
+                  :icon="$route.path === '/services' ? 'briefcase-fill' : 'briefcase'">
+                </b-icon> 
+                Services
+              </b-nav-item>
+
+              <!-- lESSONS PAGE -->
               <b-nav-item 
                 href="#"
                 :class="$route.path === '/lessons' ? 'active font-weight-bold' : ''"
@@ -44,6 +78,8 @@
                 </b-icon>
                 Lessons
               </b-nav-item>
+
+              <!-- TEACHER PAGE -->
               <b-nav-item 
                 href="#"
                 :class="$route.path === '/teachers' ? 'active font-weight-bold' : ''"
@@ -55,24 +91,52 @@
                 </b-icon> 
                 Teachers
               </b-nav-item>
-               <b-nav-item 
+
+
+              <!-- CONTACT PAGE -->
+              <b-nav-item 
                 href="#"
                 :class="$route.path === '/contacts' ? 'active font-weight-bold' : ''"
                 to="/contacts"
+                v-if="!isLoggedIn"
               >
                <b-icon 
                 :icon="$route.path === '/contacts' ? 'chat-square-dots-fill' : 'chat-square-dots'">
               </b-icon>
                 Contacts
               </b-nav-item>
+
+              <!-- PROFILE PAGE -->
+              <b-nav-item 
+                href="#"
+                :class="$route.path === '/profile' ? 'active font-weight-bold' : ''"
+                to="/profile"
+                v-if="isLoggedIn"
+              >
+               <b-icon 
+                :icon="$route.path === '/profile' ? 'person-fill' : 'person'">
+              </b-icon>
+                Profile
+              </b-nav-item>
+
+              <!-- REGISTER BUTTON -->
               <b-nav-item v-if="!isLoggedIn">
                 <b-button variant="outline-primary" size="sm" v-b-modal.register-modal>
                   <b-icon icon="person-plus-fill"></b-icon> Register
                 </b-button>
               </b-nav-item>
+
+              <!-- LOGIN BUTTON -->
               <b-nav-item v-if="!isLoggedIn">
                 <b-button variant="outline-primary" size="sm" v-b-modal.login-modal>
                   <b-icon icon="lock-fill"></b-icon> Login
+                </b-button>
+              </b-nav-item>
+
+              <!-- LOGOUT BUTTON -->
+              <b-nav-item v-if="isLoggedIn">
+                <b-button variant="primary" size="sm" @click="onClickLogout">
+                  <b-icon icon="box-arrow-in-right"></b-icon> Logout
                 </b-button>
               </b-nav-item>
             </b-navbar-nav>
@@ -92,6 +156,8 @@
 
   import { auth } from '@/services'
 
+  import { toastAlertStatus } from '@/utils'
+
   export default {
     name: 'nav-bar',
 
@@ -102,8 +168,22 @@
     
     data () {
       return {
-        toggle: false,
-        isLoggedIn: auth.currentUser
+        toggle: false
+      }
+    },
+
+    computed: {
+      isLoggedIn () {
+        return auth.currentUser
+      }
+    },
+
+    methods: {
+      onClickLogout () {  
+        auth
+         .signOut()
+         .then(() => this.$router.push('/'))
+         .catch(error => toastAlertStatus(error, 'error'))
       }
     }
   }
