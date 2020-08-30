0eee6fa5ab168fe4e02f90d3332f71e390616e73
acatzk <joshuaimalay@gmail.com>
2020-08-30 08:29:07 +0800

    Movd the Home vue component inside the public folder

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('@/views/Home')
+    component: () => import('@/views/public/Home')
   },
   {
     path: '/about',
