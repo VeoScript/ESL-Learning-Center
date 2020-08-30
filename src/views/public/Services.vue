edc5f769c0d699921ab1ae6b24a1f83fb5b08d2d
acatzk <joshuaimalay@gmail.com>
2020-08-30 11:16:23 +0800

    Added Services content

--- src/views/public/Services.vue
+++ src/views/public/Services.vue
@@ -1,15 +1,34 @@
 <template>
-    <b-container>
+    <div>
+        <service-front-page 
+            :sideImage="frontpage.sideImage"
+            :title="frontpage.title"
+            :description="frontpage.description"
+            :buttonText="frontpage.buttonText"
+        ></service-front-page> <!-- First Page to See Front Page -->
+
         <service-page
         ></service-page>
-    </b-container>
+    </div>
 </template>
 
 <script>
     export default {
         name: 'services',
 
+        data () {
+            return {
+                frontpage: {
+                    sideImage: require('@/assets/photos/communication.png'),
+                    title: 'Our Services',
+                    description: 'Learning English Language Through Video Calls. Putting Children First. Preparing children for success in life.',
+                    buttonText: 'Learn More'
+                }
+            }
+        },
+
         components: {
+            ServiceFrontPage: () => import('@/components/mixins/FrontPage'),
             ServicePage: () => import('@/components/pages/service/ServicePage')
         }
     }
