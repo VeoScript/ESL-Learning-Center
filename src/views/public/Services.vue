dbccdf4e0528011c5b64f077d90bf8fa5e095f7e
acatzk <joshuaimalay@gmail.com>
2020-08-30 10:17:23 +0800

    Import ServicePage vue component

---
+++ src/views/public/Services.vue
@@ -0,0 +1,16 @@
+<template>
+    <b-container>
+        <service-page
+        ></service-page>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        name: 'services',
+
+        components: {
+            ServicePage: () => import('@/components/pages/service/ServicePage')
+        }
+    }
+</script>
